92779e0a093d1bcb47ff79e4275becf3933e6696
J._.N._.TAPANIYA <jenish.tapaniya123@gmail.com>
2025-12-27 19:07:59 +0530

    Add files via upload

---
+++ c_sharp_2025/strg.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class strg
+  {
+    static void Main(string[] args)
+    {
+      int a= 15;
+      string b= "jenish";
+      Console.WriteLine(a);
+      Console.WriteLine(b);
+     }
+  }
+}
---
+++ c_sharp_2025/string.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "jenish";
+      Console.WriteLine(name);  
+     }
+  }
+}
