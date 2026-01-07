e2ac7f5349c7c6091e66101dc74f17a42c6a5f91
J._.N._.TAPANIYA <jenish.tapaniya123@gmail.com>
2026-01-07 16:05:22 +0530

    Add files via upload

---
+++ c_sharp_2025/arr_length.cs
@@ -0,0 +1,13 @@
+using System;
+class arr_length
+{
+	static void Main(String[] args)
+	{
+		int[] a={3,2,2,6,};
+		for(int i=0; i<a.Length; i++)
+		{
+			Console.WriteLine(a[i]);
+		}
+		Console.ReadLine();
+	}
+}
---
+++ c_sharp_2025/smpl_arr.cs
@@ -0,0 +1,13 @@
+using System;
+class smpl_arr
+{
+	static void Main(String[] args)
+	{
+		int[] a={2,3,4,5,6,};
+		for(int i=0; i<5; i++)
+		{
+			Console.WriteLine(a[i]);
+		}
+		Console.ReadLine();
+	}
+}
